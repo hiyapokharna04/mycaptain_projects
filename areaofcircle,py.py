8ee7f1ef11293e4553b3e014769bce973c3672c3
hiyapokharna04 <120382037+hiyapokharna04@users.noreply.github.com>
2022-12-14 18:23:56 +0530

    Add files via upload

---
+++ areaofcircle,py.py
@@ -0,0 +1,8 @@
+#Defining the value of pi
+pi=3.14159
+#Taking variable as radius
+radius=float(input("Radius of the circle:"))
+#Storing calculated area in variable area
+area=pi*radius**2
+#Printing the final area from the input radius
+print("The area of the circle is:",area)
