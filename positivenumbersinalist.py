919ac01f1962da9acdfdfde54705457c0a4fcf00
hiyapokharna04 <120382037+hiyapokharna04@users.noreply.github.com>
2022-12-15 20:34:45 +0530

    Add files via upload

--- positivenumbersinalist.py
+++ positivenumbersinalist.py
@@ -1,8 +1,8 @@
 li=[]
 n=int(input("Enter size of list"))
 for i in range(0,n):
-    e=int(input("Enter element of list"))
-    li.append(e)
+    x=int(input("Enter element of list"))
+    li.append(x)
     
     print("Positive numbers in",li,"are:")
     for i in li:
