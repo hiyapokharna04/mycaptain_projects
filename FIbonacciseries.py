33cc3bea5082d3dd81b1033742f72ed276a1d425
hiyapokharna04 <120382037+hiyapokharna04@users.noreply.github.com>
2022-12-15 22:00:14 +0530

    Add files via upload

---
+++ FIbonacciseries.py
@@ -0,0 +1,23 @@
+num= int(input("Enter any number:"))
+n1,n2= 0,1 
+sum=0
+if num <=0:
+    print("Invalid!")
+    
+
+else:
+        for i in range(0,num):
+            print(sum,end= ' ')
+            n1=n2
+            n2=sum
+            sum=n1+n2
+    
+        
+        
+        
+    
+        
+    
+    
+    
+
